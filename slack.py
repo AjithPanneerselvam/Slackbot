cef531510a7b6a682800b0d028ab2dd385664977
AjithPanneerselvam <ajith.ark1@gmail.com>
2016-06-21 01:14:12 +0530

    new

--- slack.py
+++ slack.py
@@ -1,14 +1,22 @@
 import os
 import time 
+import requests
 from slackclient import SlackClient 
 
 #BOT_ID = str(os.environ.get("BOT_ID"))
 BOT_ID = "<@U1JFV9U49>:"
-
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
 def handle_command(message, channel):
+	#data = requests.get('http://api.topcoder.com/v2/data/srm/contests')
+	#print data.json()
+	
+	from urllib2 import Request, urlopen
+	request = Request('http://api.topcoder.com/v2/data/srm/contests')
+	response_body = urlopen(request).read()
+	print response_body
+	
 	payload = "Done!"
 	if BOT_ID.find(message)!= -1:
 		slack_client.api_call("chat.postMessage", channel=channel, text=payload, as_user=True)
