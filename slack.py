3962aa1543b48daf083aa54898f70d7887b370f6
AjithPanneerselvam <ajith.ark1@gmail.com>
2016-06-21 00:03:43 +0530

    interim

--- slack.py
+++ slack.py
@@ -1,19 +1,52 @@
-from slackclient import SlackClient
-import time
+import os
+import time 
+from slackclient import SlackClient 
 
-token = "xoxp-52271374178-52266809857-52436717348-4372e03f1a"
-sc = SlackClient(token)
-print sc.api_call("api.test")
-print sc.api_call("channels.info", channel="")
-print sc.api_call(
-	"chat.postMessage",channel="#general",text="Hello guys, This is topcoder bot",
-	username='topcoder bot',icon_emoji=':robot_face:'
-)
+BOT_ID = os.environ.get("BOT_ID")
 
-if sc.rtm_connect():
-	while True:
-		print sc.rtm_read()
-		time.sleep(1)
+#constants
+AT_BOT = "<@" + BOT_ID + ">:"
+COMMAND = "<@" + BOT_ID + ">:"
 
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+def handle_command(command, channel):
+	print "&&&&&& this is handle func"
+	print command
+	payload = "Done!"
+	if "<@U1JFV9U49>:" in command:
+		slack_client.api_call("chat.postMessage", channel=channel, text=payload, as_user=True)
+
+		
+def parse_slack_output(slack_rtm_output):
+	
+	print "....../////...../////// the read output is ...///...."
+	print slack_rtm_output
+	output_list = slack_rtm_output
+	if output_list and len(output_list) > 0:
+		for output in output_list:
+			if output and 'text' in output:
+				print ".\n\n************ the channel is *****  " + output['channel']
+				print output['text']
+				return output['text'], output['channel']
+
+	return None, None
+
+
+
+if __name__ == '__main__':
+
+	READ_WEBSOCKET_DELAY = 1
+
+	if slack_client.rtm_connect():
+		print "StarBot connected and running!"
+		
+		while True:
+			command, channel = parse_slack_output(slack_client.rtm_read())
+			
+			if command and channel:
+				handle_command(command, channel)	
+			time.sleep(READ_WEBSOCKET_DELAY)
+			
 	else:
-		print "Connection Failed, invalid token?"
+		print "Connection failed. Invalid Slack token or bot ID?"	
